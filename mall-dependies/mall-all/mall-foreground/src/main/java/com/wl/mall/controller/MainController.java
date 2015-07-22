1e5b815e7e971b7e0440705fe4ffe412575b3354
wrs <252797991@qq.com>
2015-07-22 23:56:48 +0800

    spring 注解试事务

--- mall-dependies/mall-all/mall-foreground/src/main/java/com/wl/mall/controller/MainController.java
+++ mall-dependies/mall-all/mall-foreground/src/main/java/com/wl/mall/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.wl.mall.foreground.controller;
+package com.wl.mall.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
--- mall-dependies/mall-all/mall-module-user/src/main/java/com/wl/mall/service/impl/UserServiceImpl.java
+++ mall-dependies/mall-all/mall-module-user/src/main/java/com/wl/mall/service/impl/UserServiceImpl.java
@@ -3,6 +3,10 @@ package com.wl.mall.service.impl;
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.wl.mall.model.User;
 import com.wl.mall.module.common.dao.BaseDao;
 import com.wl.mall.service.UserService;
@@ -17,7 +21,7 @@ public class UserServiceImpl implements UserService {
 	 * @see com.wl.mall.service.impl.UserService#saveUser(com.wl.mall.model.User)
 	 */
 	@Override
-
+@Transactional(propagation=Propagation.REQUIRED, isolation=Isolation.READ_COMMITTED)
 public void saveUser(User user) {
 	baseDao.save(user);
 }
